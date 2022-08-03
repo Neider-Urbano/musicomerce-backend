bc6bad679ce06def37e959b593138ae932efff77
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2022-08-03 17:56:06 -0300

    ruta deleteInstrument creada

--- src/routes/instrumentsRoutes.js
+++ src/routes/instrumentsRoutes.js
@@ -3,13 +3,14 @@ const router = Router();
 const { Instrument } = require('../db');
 const { Category } = require('../db');
 const {Op} = require('sequelize');
-const { postInstrument, getInstrument,deleteInstrument } = require("../controllers/InstrumentsController.js");
+const { postInstrument, getInstrument,deleteInstrument,putInstrument } = require("../controllers/InstrumentsController.js");
 
 
 
 router.post("/", postInstrument);
 router.get("/", getInstrument);
 router.delete("/:id", deleteInstrument);
+router.put("/",putInstrument);
 
 
 
