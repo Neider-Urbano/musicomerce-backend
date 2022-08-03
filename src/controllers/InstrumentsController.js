03ff93f804156e9b8bf931a341f438f597b0af35
oscar.restrepo@gmail.com <oscar.restrepo2@gmail.com>
2022-08-03 16:38:14 -0500

    feat(controllers/instrument && routes/instrument): ruta y controlador creados

--- src/controllers/InstrumentsController.js
+++ src/controllers/InstrumentsController.js
@@ -1,82 +1,107 @@
-const {Instrument, Category} = require("../db.js");
-const {Op} = require('sequelize');
-
-
+const { Instrument, Category } = require("../db.js");
+const { Op } = require("sequelize");
 
 const postInstrument = async (req, res, next) => {
-    const {name, brand, price, img, description, stock, status, category} = req.body;
-    try {
-        let newInstrumentCategory = await Category.findOne({
-            where:{
-                name: category
-            }
-        })
-        if(!newInstrumentCategory){
-            throw new TypeError("Category not found")
-        }else{  
-            let newInstrument  = await Instrument.create({
-                name, 
-                brand, 
-                price, 
-                img, 
-                description, 
-                stock, 
-                status
-            })
-            await newInstrument.setCategory(newInstrumentCategory);
-            return res.status(200).json({message: "Activity successfully added"});
-        }
-    } catch (error) {
-        next(error);
+  const { name, brand, price, img, description, stock, status, category } =
+    req.body;
+  try {
+    let newInstrumentCategory = await Category.findOne({
+      where: {
+        name: category,
+      },
+    });
+    if (!newInstrumentCategory) {
+      throw new TypeError("Category not found");
+    } else {
+      let newInstrument = await Instrument.create({
+        name,
+        brand,
+        price,
+        img,
+        description,
+        stock,
+        status,
+      });
+      await newInstrument.setCategory(newInstrumentCategory);
+      return res.status(200).json({ message: "Activity successfully added" });
     }
-}
-
+  } catch (error) {
+    next(error);
+  }
+};
 
 const getInstrument = async (req, res, next) => {
-    try {
-    let name=req.query.name
-    if(name){
-        let instrumentos=await Instrument.findAll({
-            where:{name:{[Op.like]:`%${name}%`}},
-            include:{model:Category} 
-        })
-        if(instrumentos.length) return res.status(200).send(instrumentos)
-        else return res.status(400).send("Instrument "+name+" not found")
-    }else{
-        let instrumentos=await Instrument.findAll({
-            include:{model:Category}
-           }) 
-        if(instrumentos.length) return res.status(200).send(instrumentos)
-        else return res.status(400).send("No Instruments to show")
+  try {
+    let name = req.query.name;
+    if (name) {
+      let instrumentos = await Instrument.findAll({
+        where: { name: { [Op.like]: `%${name}%` } },
+        include: { model: Category },
+      });
+      if (instrumentos.length) return res.status(200).send(instrumentos);
+      else return res.status(400).send("Instrument " + name + " not found");
+    } else {
+      let instrumentos = await Instrument.findAll({
+        include: { model: Category },
+      });
+      if (instrumentos.length) return res.status(200).send(instrumentos);
+      else return res.status(400).send("No Instruments to show");
     }
-    } catch (error) {
-        next(error);
-    }
-}
+  } catch (error) {
+    next(error);
+  }
+};
 
 const deleteInstrument = async (req, res) => {
-    const {id}=req.params;
-    try {
-        if(id){
-            const deleteInstrument=await Instrument.destroy({
-                where: {id: id}
-            })
-            if(!deleteInstrument){
-                throw new TypeError("Error, instrument not found with this Id")
-            }
-            res.status(200).send("Instrument deleted")
-        }else{
-            throw new TypeError("Error, The Id entered is not valid")
-        }
-    } catch (e) {
-        return res.status(400).send(e.message);
+  const { id } = req.params;
+  try {
+    if (id) {
+      const deleteInstrument = await Instrument.destroy({
+        where: { id: id },
+      });
+      if (!deleteInstrument) {
+        throw new TypeError("Error, instrument not found with this Id");
+      }
+      res.status(200).send("Instrument deleted");
+    } else {
+      throw new TypeError("Error, The Id entered is not valid");
     }
-}
+  } catch (e) {
+    return res.status(400).send(e.message);
+  }
+};
 
+const getIdInstrument = async (req, res) => {
+  const { id } = req.params;
+  const totalInstruments = await getInstrument();
 
+  try {
+    const idInstrument = await totalInstruments.filter(
+      (instrument) => instrument.id == id
+    );
+    const instrument = idInstrument.map((instrument) => {
+      return {
+        name: instrument.name,
+        brand: instrument.brand,
+        price: instrument.price,
+        img: instrument.img,
+        description: instrument.description,
+        stock: instrument.stock,
+        status: instrument.status,
+      };
+    });
+
+    idInstrument.length
+      ? res.status(200).send(instrument)
+      : res.status(404).send(`Id ${id} not found`);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    postInstrument,
-    getInstrument,
-    deleteInstrument,
-}
+  postInstrument,
+  getInstrument,
+  deleteInstrument,
+  getIdInstrument,
+};
--- src/routes/instrumentsRoutes.js
+++ src/routes/instrumentsRoutes.js
@@ -1,16 +1,18 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { Instrument } = require('../db');
-const { Category } = require('../db');
-const {Op} = require('sequelize');
-const { postInstrument, getInstrument,deleteInstrument } = require("../controllers/InstrumentsController.js");
-
-
+const { Instrument } = require("../db");
+const { Category } = require("../db");
+const { Op } = require("sequelize");
+const {
+  postInstrument,
+  getInstrument,
+  deleteInstrument,
+  getIdInstrument,
+} = require("../controllers/InstrumentsController.js");
 
 router.post("/", postInstrument);
 router.get("/", getInstrument);
 router.delete("/:id", deleteInstrument);
-
-
+router.get("/:id", getIdInstrument);
 
 module.exports = router;
