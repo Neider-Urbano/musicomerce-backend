55159d430c6695ffd56398887cf0e2594fc948a5
oscar.restrepo@gmail.com <oscar.restrepo2@gmail.com>
2022-08-03 20:06:26 -0500

    feat/ creacion bulkcreate alimenta base de datos

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const { allData } = require("./src/bulkCreate.js");
 
-conn.sync({ force: true }).then(() => {
-  server.listen(3000, () => {
-    console.log('%s listening at 3000');
-  });
+conn.sync({ force: true }).then(async () => {
+  try {
+    await allData();
+    console.log("Data loaded");
+
+    server.listen(3000, () => {
+      console.log("%s listening at 3000");
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
