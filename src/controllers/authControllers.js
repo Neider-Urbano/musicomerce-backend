5e2cc40fd655dfc6458ee2f2ee897df7fe8bec38
Ezequiel Antonio Soto <Ezequiel_soto_f4@hotmail.com>
2022-08-11 17:49:18 -0300

    ruta users/token devuelve el usuario si el token es valido

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -4,10 +4,10 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const controllerRegister=async(req, res)=>{
+const controllerRegister = async (req, res) => {
   try {
-    const { userName,email, password } = req.body;
-    if(!email || !password || !userName){ throw new Error("Password, userName or Email is required")}
+    const { userName, email, password } = req.body;
+    if (!email || !password || !userName) { throw new Error("Password, userName or Email is required") }
     const user = await User.findOne({ where: { email: email } });
 
     if (user)
@@ -19,7 +19,7 @@ const controllerRegister=async(req, res)=>{
   }
 }
 
-const controllerLogin=async(req, res)=>{
+const controllerLogin = async (req, res) => {
   try {
     const { userName, password } = req.body;
 
@@ -36,18 +36,39 @@ const controllerLogin=async(req, res)=>{
     if (!comparePassword) {
       throw new TypeError("Username or Password invalid.");
     }
-    const token = jwt.sign(
-      { user_id: user.id, user_rol: user.rol }, 
-      process.env.JWT_SECRET
-    );
+    const token = jwt.sign({ user_id: user.id, user_rol: user.rol },process.env.JWT_SECRET);
 
     return res.status(200).json({ token: token });
   } catch (error) {
     return res.status(404).send(error.message);
   }
 }
+const verifyToken = async (req, res, next) => {
+  try {
+    const BearesToken = req.get("Authorization");
+
+    if (!BearesToken) return res.status(401).send({ error: "token not found" })
+
+    const token = BearesToken.replace("Bearer ", "")
+    try {
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      if(!decoded.user_id) return res.status(401).send({error:"id not found"})
+      req.user_id = decoded.user_id;
+      next();
+    } catch (e) {
+      console.log(e)
+      return res.status(400).send(e)
+    }
+
+  } catch (e) {
+    console.log(e)
+    return res.status(400).send(e)
+  }
+}
 
 module.exports = {
-    controllerLogin,
-    controllerRegister
+  controllerLogin,
+  controllerRegister,
+  verifyToken
 };
