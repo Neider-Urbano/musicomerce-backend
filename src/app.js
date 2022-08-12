328cd627ae469c28d19d0f93f46fe3d471d142b0
Nilson Max <nilsonbenitez@hotmail.com>
2022-08-12 11:29:06 -0500

    controllers/stripecontrollers se agrego este controllers y en routers/apps se agrego cors para conectar apixtripe desde el back con el front y se agrego routers/pyementroters

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+const cors = require("cors");
+
 
 require('./db.js');
 
 const server = express();
 
 server.name = 'API';
+//payment_method
+server.use(cors({ origin: "http://localhost:3000" }));
+// server.use(express.json());
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(express.urlencoded({ extended: true, limit: '50mb' }));
+server.use(express.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
--- src/controllers/stripeController.js
+++ src/controllers/stripeController.js
@@ -1,47 +1,70 @@
 const Stripe = require("stripe");
 require("dotenv").config();
+
 const stripe = Stripe(process.env.REACT_PUBLIC_STRIPE_SECRET_KEY);
-  const handler = async (req, res)=>{
-    try {
-      const params = {
-        submit_type: "pay",
-        mode: "payment",
-        payment_method_types: ["card"],
-        billing_address_collection: "auto",
-        shipping_options: [],
-        line_items: req.body.map((item) => {
-          const img = item.img
-          const newImage = img
-          return {
-            price_data: {
-              currency: "usd",
-              product_data: {
-                name: item.name,
-                description: item.description,
-                images: [newImage],
-              },
-              unit_amount: item.price * 100,
-            },
-            adjustable_quantity: {
-              enabled: true,
-              minimum: 1,
-            },
-            quantity: item.quantity,
-          };
-        }),
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
-      };
-      // Create Checkout Sessions from body params.
-      const session = await stripe.checkout.sessions.create(params);
-      res.status(200).json(session);
-      console.log(session);
-    } catch (err) {
-      res
-        .status(err.statusCode || 500)
-        .json({ statusCode: 500, message: err.message });
-    }
+// const stripe = new Stripe("sk_test_51LVjV6Ci4QJcVHNo9FDkoRiwpryJcbGe19W9eTDNtHhEnXPfIwBvDZ06TSJtruRxQ2ZqoZXcmvcDjclTaOxcgzxj00qHHLuuHh");
+
+const handler = async (req, res) => {
+console.log('estoy en el back')
+  const { id, amount } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Gaming Keyboard",
+      payment_method: id,
+      confirm: true, //confirm the payment at the same time
+    });
+
+    console.log(payment);
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+    console.log(error, 'estoy en error back');
+    return res.json({ message: error.raw.message });
   }
- 
+
+  // try {
+  //   const params = {
+  //     submit_type: "pay",
+  //     mode: "payment",
+  //     payment_method_types: ["card"],
+  //     billing_address_collection: "auto",
+  //     shipping_options: [],
+  //     line_items: req.body.map((item) => {
+  //       const img = item.img
+  //       const newImage = img
+  //       return {
+  //         price_data: {
+  //           currency: "usd",
+  //           product_data: {
+  //             name: item.name,
+  //             description: item.description,
+  //             images: [newImage],
+  //           },
+  //           unit_amount: item.price * 100,
+  //         },
+  //         adjustable_quantity: {
+  //           enabled: true,
+  //           minimum: 1,
+  //         },
+  //         quantity: item.quantity,
+  //       };
+  //     }),
+  //     success_url: `${req.headers.origin}/success`,
+  //     cancel_url: `${req.headers.origin}/?canceled=true`,
+  //   };
+  //   // Create Checkout Sessions from body params.
+  //   const session = await stripe.checkout.sessions.create(params);
+  //   res.status(200).json(session);
+  //   console.log(session);
+  // } catch (err) {
+  //   res
+  //     .status(err.statusCode || 500)
+  //     .json({ statusCode: 500, message: err.message });
+  // }
+}
+
 
 module.exports = handler;
