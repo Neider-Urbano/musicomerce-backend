fc66b1160bf67c73a71b67ca2c1ec8a507361aaa
oscar.restrepo@gmail.com <oscar.restrepo2@gmail.com>
2022-08-14 23:18:45 -0500

    fix user controllers

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,15 +1,18 @@
-const {verifyToken}=require("../middlewares/authjwt")
+const { verifyToken } = require("../middlewares/authjwt");
 const { Router } = require("express");
 const router = Router();
 
 const {
-    postUser,deleteUser,putUser,
-    postUsersAll,getUsers,getUserToken,
+  postUser,
+  deleteUser,
+  putUser,
+  postUsersAll,
+  getUsers,
+  getUserToken,
 } = require("../controllers/usersController");
 
-
 router.get("/", getUsers);
-router.get("/token",[verifyToken], getUserToken);
+router.get("/token", [verifyToken], getUserToken);
 router.post("/all", postUsersAll);
 router.post("/", postUser);
 router.delete("/:id", deleteUser);
