b55fa9ad38a84458eff12165132482b88207c98b
Neider Urbano <jjuly8julianur@gmail.com>
2022-08-20 14:29:56 -0500

    admin actualizacion

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -14,6 +14,6 @@ router.get("/",[verifyTokenAdmin], getAdmins);
 router.post("/register",[verifyTokenAdmin], createtAdmin);
 router.delete("/", [verifyTokenAdmin],deleteAdmin);
 router.put("/",[verifyTokenAdmin], modifyAdmin);
-router.get("/:id",[verifyTokenAdmin], getAdminById);
+router.get("/id",[verifyTokenAdmin], getAdminById);
 
 module.exports = router;
