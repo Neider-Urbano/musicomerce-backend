4dd3c310a70c6210bea4c290fa7c3e603ff7f180
AngieVaMo <anvamopa2@gmail.com>
2022-08-23 15:12:53 -0500

    cambio de idioma para el mailing

--- src/mailing/estructure.js
+++ src/mailing/estructure.js
@@ -18,9 +18,9 @@ class mailStructure {
   setHtmlRegister(userName, password) {
     this.html = `
     <div>
-            <h1>Bienvenid@ a MusiCommerce</h1>
+            <h1>Welcome to MusiCommerce</h1>
             <div>
-              <h3>Este es un resumen de tu información</h3>
+              <h3>Summary of your information:</h3>
               <ul>
                 <li>Username: ${userName}</li>
                 <li>Password: ${password}</li>
@@ -31,17 +31,17 @@ class mailStructure {
   }
   setHtmlShop(name, phone, address, city, country, zip, total, items) {
     this.html = `
-    <h2 style="color:#2B4570">RESUMEN DE TU COMPRA</h2>
+    <h2 style="color:#2B4570">PURCHASE SUMMARY</h2>
     </br>
     <div>
-      <h3 style="color:#2B4570">DATOS DE ENVÍO</h3>
+      <h3 style="color:#2B4570">SHIPPING INFORMATION</h3>
       </p>
-      <p><b style="color:#5497A7">Nombre: </b>${name}</p>
-      <p><b style="color:#5497A7">Telefono: </b>${phone}</p>
-      <p><b style="color:#5497A7">Dirección: </b>${address}</p>
-      <p><b style="color:#5497A7">Ciudad: </b>${city}</p>
-      <p><b style="color:#5497A7">Pais: </b>${country}</p>
-      <p><b style="color:#5497A7">Zip: </b>${zip}</p>
+      <p><b style="color:#5497A7">Name: </b>${name}</p>
+      <p><b style="color:#5497A7">Phone: </b>${phone}</p>
+      <p><b style="color:#5497A7">Address: </b>${address}</p>
+      <p><b style="color:#5497A7">City: </b>${city}</p>
+      <p><b style="color:#5497A7">Country: </b>${country}</p>
+      <p><b style="color:#5497A7">Zip Code: </b>${zip}</p>
     </div>
     </br>
     </br>
@@ -50,16 +50,16 @@ class mailStructure {
       <div>${items.map(
         (e) =>
           `<img src= ${e.img} style= "height:150px" "width=150px"/>
-        <p><b style="color:#5497A7">Instrumento:</b> ${e.name}</p>
-        <p><b style="color:#5497A7">Unidades:</b> ${e.count}</p>
-        <p><b style="color:#5497A7">Precio Unitario:</b> $${e.price}</p>
+        <p><b style="color:#5497A7">Instrument:</b> ${e.name}</p>
+        <p><b style="color:#5497A7">Units:</b> ${e.count}</p>
+        <p><b style="color:#5497A7">unit price:</b> $${e.price}</p>
         `
       )}
       </div>
     </div>
     </br>
     </br>
-    <h2 style="color:#2B4570"> Toal pagado: $${total}</h2>`;
+    <h2 style="color:#2B4570"> Total amount paid: $${total}</h2>`;
   }
 
   setHtmlUpdate(
@@ -72,14 +72,14 @@ class mailStructure {
   ) {
     this.html = `
     <div>
-            <h1 style="color:#2B4570">HAS ACTUALIZADO TU PERFIL</h1>
+            <h1 style="color:#2B4570">YOU HAVE UPDATED YOUR PROFILE</h1>
             <div>
-              <h3 style="color:#2B4570" >Este es un resumen de tus cambios</h3>
+              <h3 style="color:#2B4570" >This is a summary of your changes</h3>
               <ul>
-                <P><b style="color:#5497A7">Nombre</b>: ${firstName}</P>
-                <P><b style="color:#5497A7">Apellido:</b> ${lastName}</P>
-                <P><b style="color:#5497A7">Numero de contacto:</b> ${contactNumber}</P>
-                <P><b style="color:#5497A7">Direccion:<b> ${buyerAddress}</P>
+                <P><b style="color:#5497A7">Name</b>: ${firstName}</P>
+                <P><b style="color:#5497A7">Last name:</b> ${lastName}</P>
+                <P><b style="color:#5497A7">Contact Number:</b> ${contactNumber}</P>
+                <P><b style="color:#5497A7">Address:<b> ${buyerAddress}</P>
               </ul>
             </div>
     </div>`;
@@ -88,8 +88,8 @@ class mailStructure {
   setNewsletter(email) {
     this.html = `
     <div>
-      <h1 style="color:#2B4570">Gracias por tu suscripción a MusiCommerce</h1>
-      <h3>Te mantendremos informad@</h3>
+      <h1 style="color:#2B4570">Thank you for your subscription to MusiCommerce</h1>
+      <h3>We will keep you informed</h3>
       <p>${email}</p>
     </div>
     `;
@@ -98,9 +98,9 @@ class mailStructure {
   setResetPassword(email, token) {
     this.html = `
     <div>
-      <h1 style="color:#2B4570">CAMBIO DE CONTRASEÑA</h1>
-      <h3 style="color:#2B4570" >Has solicitado un cambio de cotraseña desde tu correo ${email}</h3>
-      <h3>Da click en el siguiente enlace:</h3>
+      <h1 style="color:#2B4570">PASSWORD CHANGE</h1>
+      <h3 style="color:#2B4570" >You have requested a password change from your email ${email}</h3>
+      <h3>Click on the following link:</h3>
       <p>http://localhost:3000/user/resetpassword</p>
     </div>
     `;
@@ -109,8 +109,8 @@ class mailStructure {
   setPassReseted(pass) {
     this.html = `
   <div>
-    <h1 style="color:#2B4570">Cambio de contraseña exitoso</h1>
-    <h4>Esta es tu nueva constraseña: </h4>
+    <h1 style="color:#2B4570">Successful password change</h1>
+    <h4>This is your new password: </h4>
     <p>${pass}</p>
   </div>
   `;
